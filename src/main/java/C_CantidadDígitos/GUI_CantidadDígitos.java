23570a98bdf9b24890e67b63e61ee4f35e531bb6
nquinbla <nquinbla@myuax.com>
2024-04-16 11:28:24 +0200

    Ej.3 Cantidad Dígitos GUI

--- src/main/java/C_CantidadDígitos/GUI_CantidadDígitos.java
+++ src/main/java/C_CantidadDígitos/GUI_CantidadDígitos.java
@@ -1,4 +1,45 @@
 package C_CantidadDígitos;
 
-public class GUI_CantidadDígitos {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_CantidadDígitos extends JFrame {
+    private JTextField textField;
+    private JButton button;
+    private JLabel label;
+
+    public GUI_CantidadDígitos() {
+        setLayout(new FlowLayout());
+
+        label = new JLabel("Introduce un número:");
+        add(label);
+
+        textField = new JTextField(10);
+        add(textField);
+
+        button = new JButton("Contar dígitos");
+        add(button);
+
+        event e = new event();
+        button.addActionListener(e);
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int num = Integer.parseInt(textField.getText());
+            CantidadDígitos cd = new CantidadDígitos();
+            int cantidad = cd.contarDigitos(num);
+            JOptionPane.showMessageDialog(null, "El número " + num + " tiene " + cantidad + " dígitos.");
+        }
+    }
+
+    public static void main(String[] args) {
+        GUI_CantidadDígitos gui = new GUI_CantidadDígitos();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.pack();
+        gui.setTitle("Cantidad de dígitos");
+    }
 }
