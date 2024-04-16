73e355ba0f7bfcb55853c585126d4df71f29335a
nquinbla <nquinbla@myuax.com>
2024-04-16 23:00:37 +0200

    Ej9. Ordenar alfabeticamente

--- src/main/java/I_OrdenarAlfabeticamente/OrdenarAlfabeticamente.java
+++ src/main/java/I_OrdenarAlfabeticamente/OrdenarAlfabeticamente.java
@@ -1,4 +1,22 @@
 package I_OrdenarAlfabeticamente;
 
+import java.io.*;
+import java.util.*;
+
 public class OrdenarAlfabeticamente {
+    public void ordenarArchivo(String archivoEntrada, String archivoSalida) throws IOException {
+        List<String> lineas = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivoEntrada))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                lineas.add(linea);
+            }
+        }
+        Collections.sort(lineas);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(archivoSalida))) {
+            for (String linea : lineas) {
+                writer.println(linea);
+            }
+        }
+    }
 }
