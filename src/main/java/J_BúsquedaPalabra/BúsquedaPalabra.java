902e4b14306ec1ec527d00f2e1a1f972645b0aa9
nquinbla <nquinbla@myuax.com>
2024-04-16 23:04:03 +0200

    Ej10. Búsuqeda de palabras

--- src/main/java/J_BúsquedaPalabra/BúsquedaPalabra.java
+++ src/main/java/J_BúsquedaPalabra/BúsquedaPalabra.java
@@ -1,4 +1,18 @@
 package J_BúsquedaPalabra;
 
+import java.io.*;
+import java.util.*;
+
 public class BúsquedaPalabra {
+    public boolean buscarPalabra(String archivo, String palabra) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                if (Arrays.asList(linea.split("\\s+")).contains(palabra)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
