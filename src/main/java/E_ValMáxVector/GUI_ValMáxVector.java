9c11ba4e222ee8ff9e5410cc55699c35f76e712c
nquinbla <nquinbla@myuax.com>
2024-04-16 22:39:03 +0200

    Ej5. Valor másimo de un vectr en numeros enteros en GUI

--- src/main/java/E_ValMáxVector/GUI_ValMáxVector.java
+++ src/main/java/E_ValMáxVector/GUI_ValMáxVector.java
@@ -1,4 +1,49 @@
 package E_ValMáxVector;
 
-public class GUI_ValMáxVector {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_ValMáxVector extends JFrame {
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JButton button;
+    private JLabel label;
+
+    public GUI_ValMáxVector() {
+        setLayout(new FlowLayout());
+
+        add(new JLabel("Valor x:"));
+        textFieldX = new JTextField(10);
+        add(textFieldX);
+
+        add(new JLabel("Valor y:"));
+        textFieldY = new JTextField(10);
+        add(textFieldY);
+
+        button = new JButton("Calcular x^y");
+        add(button);
+
+        event e = new event();
+        button.addActionListener(e);
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int x = Integer.parseInt(textFieldX.getText());
+            int y = Integer.parseInt(textFieldY.getText());
+            ValMáxVector valMáxVector = new ValMáxVector();
+            int resultado = valMáxVector.calcularPotencia(x, y);
+            JOptionPane.showMessageDialog(null, "El resultado de " + x + "^" + y + " es " + resultado + ".");
+        }
+    }
+
+    public static void main(String[] args) {
+        GUI_ValMáxVector gui = new GUI_ValMáxVector();
+        gui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        gui.setVisible(true);
+        gui.pack();
+        gui.setTitle("Cálculo de x^y");
+    }
 }
