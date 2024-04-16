bb3a73ee357e9093d2b14e2224a0016da78de898
nquinbla <nquinbla@myuax.com>
2024-04-16 22:38:51 +0200

    Ej5. Valor másimo de un vectr en numeros enteros

--- src/main/java/E_ValMáxVector/ValMáxVector.java
+++ src/main/java/E_ValMáxVector/ValMáxVector.java
@@ -1,4 +1,11 @@
 package E_ValMáxVector;
 
 public class ValMáxVector {
+    public int calcularPotencia(int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * calcularPotencia(x, y - 1);
+        }
+    }
 }
