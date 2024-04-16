72c616cebe0d19e47502f403d93298f63cd56599
nquinbla <nquinbla@myuax.com>
2024-04-16 22:45:04 +0200

    ajustes Ej6

--- src/main/java/F_ValMáxVectCompNum/GUI_ValMáxVectCompNum.java
+++ src/main/java/F_ValMáxVectCompNum/GUI_ValMáxVectCompNum.java
@@ -1,4 +1,42 @@
 package F_ValMáxVectCompNum;
 
-public class GUI_ValMáxVectCompNum {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_ValMáxVectCompNum extends JFrame {
+    private JTextField textFieldHex;
+    private JButton button;
+
+    public GUI_ValMáxVectCompNum() {
+        setLayout(new FlowLayout());
+
+        add(new JLabel("Valor hexadecimal:"));
+        textFieldHex = new JTextField(10);
+        add(textFieldHex);
+
+        button = new JButton("Convertir a decimal");
+        add(button);
+
+        event e = new event();
+        button.addActionListener(e);
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String hex = textFieldHex.getText();
+            ValMáxVectCompNum hexToDec = new ValMáxVectCompNum();
+            int resultado = hexToDec.ValMáxVectCompNum(hex, hex.length());
+            JOptionPane.showMessageDialog(null, "El valor decimal de " + hex + " es " + resultado + ".");
+        }
+    }
+
+    public static void main(String[] args) {
+        GUI_ValMáxVectCompNum gui = new GUI_ValMáxVectCompNum();
+        gui.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        gui.setVisible(true);
+        gui.pack();
+        gui.setTitle("Conversión de hexadecimal a decimal");
+    }
 }
--- src/main/java/F_ValMáxVectCompNum/ValMáxVectCompNum.java
+++ src/main/java/F_ValMáxVectCompNum/ValMáxVectCompNum.java
@@ -1,4 +1,11 @@
 package F_ValMáxVectCompNum;
 
 public class ValMáxVectCompNum {
+    public int  ValMáxVectCompNum(String hex, int length) {
+        if (length == 1) {
+            return Character.digit(hex.charAt(0), 16);
+        } else {
+            return Character.digit(hex.charAt(length - 1), 16) + 16 * ValMáxVectCompNum(hex, length - 1);
+        }
+    }
 }
