a005c5e572c288b5a5ecac423e361602a823dad5
nquinbla <nquinbla@myuax.com>
2024-04-16 11:25:16 +0200

    Ej.2 Funcion recursiva lista entre dos valores 2

--- src/main/java/B_ListaNumDosVal/ListaNumDosVal.java
+++ src/main/java/B_ListaNumDosVal/ListaNumDosVal.java
@@ -1,4 +1,10 @@
 package B_ListaNumDosVal;
 
 public class ListaNumDosVal {
+    public void imprimirNumeros(int a, int d) {
+        if (a <= d) {
+            System.out.println(a);
+            imprimirNumeros(a + 1, d);
+        }
+    }
 }
