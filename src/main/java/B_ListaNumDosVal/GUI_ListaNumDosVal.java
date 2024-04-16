c149c2f3090b0908525d5007c8361edb986efe9c
nquinbla <nquinbla@myuax.com>
2024-04-16 11:26:08 +0200

    Ej.2 Funcion recursiva lista entre dos valores 2 GUI

--- src/main/java/B_ListaNumDosVal/GUI_ListaNumDosVal.java
+++ src/main/java/B_ListaNumDosVal/GUI_ListaNumDosVal.java
@@ -1,4 +1,59 @@
 package B_ListaNumDosVal;
 
-public class GUI_ListaNumDosVal {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_ListaNumDosVal extends JFrame {
+    private JTextField textFieldA;
+    private JTextField textFieldD;
+    private JButton button;
+    private JTextArea textArea;
+
+    public GUI_ListaNumDosVal() {
+        setLayout(new FlowLayout());
+
+        add(new JLabel("Valor a:"));
+        textFieldA = new JTextField(10);
+        add(textFieldA);
+
+        add(new JLabel("Valor d:"));
+        textFieldD = new JTextField(10);
+        add(textFieldD);
+
+        button = new JButton("Imprimir números");
+        add(button);
+
+        textArea = new JTextArea(5, 20);
+        add(new JScrollPane(textArea));
+
+        event e = new event();
+        button.addActionListener(e);
+    }
+
+    public class event implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int a = Integer.parseInt(textFieldA.getText());
+            int d = Integer.parseInt(textFieldD.getText());
+            ListaNumDosVal lndv = new ListaNumDosVal();
+            textArea.setText("");
+            imprimirNumerosEnTextArea(a, d, lndv);
+        }
+
+        private void imprimirNumerosEnTextArea(int a, int d, ListaNumDosVal lndv) {
+            if (a <= d) {
+                textArea.append(a + "\n");
+                imprimirNumerosEnTextArea(a + 1, d, lndv);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        GUI_ListaNumDosVal gui = new GUI_ListaNumDosVal();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.pack();
+        gui.setTitle("Lista de números entre dos valores");
+    }
 }
